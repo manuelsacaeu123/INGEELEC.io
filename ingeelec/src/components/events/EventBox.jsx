fb3fd572dbf4d5ae19e33da113b32a987edf8f92
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-05 18:17:19 -0400

    mostrando los eventos
    eventos

--- ingeelec/src/components/events/EventBox.jsx
+++ ingeelec/src/components/events/EventBox.jsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+
+
+
 const EventBox = ({ events }) => {
     return (
         <div className='bg-secondary/60 rounded-[10px] p-4 xl:p-12 relative'>
@@ -15,24 +18,36 @@ const EventBox = ({ events }) => {
                     />
                 </div>
                 {/* event list */}
-                <div className='flex-1 bg-purple-400/10 h-[500px] flex flex-col justify-between overflow-y-scroll scrollbar-thin scrollbar-thumb-accent
-scrollbar-track-white/10 xl:pr-6'>
+                <div className='flex-1 bg-purple-400/10 h-[500px] flex flex-col justify-between overflow-y-scroll scrollbar-thin
+                 scrollbar-thumb-accentscrollbar-track-white/10 xl:pr-6'>
                     {/* event item */}
                     {events.map((event) => {
                         return (
-                            <div key={event.id}>
-                                <div>
+                            <div
+                                key={event.id}
+                                className='flex flex-col xl:flex-row items-center justify-between
+                                gap-y-4 xl:gap-y-0 text-center xl:text-left my-4 xl:my-0 border-b
+                                border-white/10 pb-10 xl:py-3 last-of-type:border-none
+                                first-of-type:pt-0'>
+
+                                <div className='flex flex-col justify-center items-center gap-x-4'>
                                     { /* day & month */}
-                                    <div className='flex flex-col justify-center items-center leading-tight w- [80px] mb-4 xl: mb-0'>
-                                        <div> {event.date.day}</div>
-                                        <div> {event.date.month}</div>
+                                    <div className='flex flex-col justify-center items-center leading-tight w-[80px] mb-4 xl:mb-0'>
+                                        <div className= 'text-[44px] font-black text-accent'> 
+                                            {event.date.day}
+                                        </div>
+                                        <div className='text-[20px] font-extrabold'> 
+                                            {event.date.month}
+                                        </div>
                                     </div>
 
                                     {/* location */}
-                                    <div>
-                                        <div> {`${event.location.city}, ${event.location.country}`}</div>
-                                        <div>
-                                            <div>location icon</div>
+                                    <div className='bg-red-500/20 w-64 flex flex-col gap-y-2'>
+                                        <div className='text-lg leading-none font-bold'> {`${event.location.city}, ${event.location.country}`}</div>
+                                        <div className='flex items-center gap-x-1'>
+                                            <div>
+
+                                            </div>
                                             <div> {event.location.address}</div>
                                         </div>
                                     </div>
@@ -40,8 +55,11 @@ scrollbar-track-white/10 xl:pr-6'>
                             </div>
                         );
                     })}
-                </div>
-            </div>
-        </div>
+                </div >
+            </div >
+        </div >
     );
 };
+
+
+export default EventBox; // Exporta EventBox como exportación por defecto
--- ingeelec/src/components/events/Events.jsx
+++ ingeelec/src/components/events/Events.jsx
@@ -1,24 +1,21 @@
-import EventBox from "./EventBox";
+import EventBox from './EventBox';
 
-const getEvents = async ()=> {
+const getEvents = async () => {
     const res = await fetch('http://localhost:4000/events');
     return res.json();
-}
+};
+
 const Events = async () => {
     const events = await getEvents();
-    console.log(events);
+    //console.log(events);
 
-    return <section className="section" id="tours">
-        <div className="container  mx-auto">
-            {
-                /*even box*/
-                <EventBox events= {events}/>
-            }
-
-        </div>
-
-    </section>
-  
-}
-
+    return (
+        <section className="section" id="tours">
+            <div className="container mx-auto">
+                {/*even box*/}
+                <EventBox events={events} />
+            </div>
+        </section>
+    );
+};
 export default Events
