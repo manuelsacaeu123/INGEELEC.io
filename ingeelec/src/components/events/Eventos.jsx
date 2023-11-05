fa3d14be2854ecbba87595e65689d5e196eb0f5d
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-05 16:42:24 -0400

    datos json

---
+++ ingeelec/src/components/events/Eventos.jsx
@@ -0,0 +1,11 @@
+
+const getEvents = async ()=> {
+    const res = await fetch('http://localhost:4000/events');
+}
+const Eventos = () => {
+  return (
+    <div>Eventos</div>
+  )
+}
+
+export default Eventos
