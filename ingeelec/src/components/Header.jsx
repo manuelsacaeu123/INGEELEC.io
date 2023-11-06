7f6e67c119879127313359c54dec7220ded8dd03
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-06 01:47:20 -0400

    menu en el header

--- ingeelec/src/components/Header.jsx
+++ ingeelec/src/components/Header.jsx
@@ -11,31 +11,54 @@ import { fadeIn } from "../../variants";
 const Header = () => {
   const [active, setActive] = useState(false);
 
-  useEffect(() =>{
+  useEffect(() => {
     const handleScroll = () => {
       // detectar scroll
       setActive(window.scrollY > 100);
     };
 
-  // agregar evento
-  window.addEventListener('scroll' , handleScroll);
+    // agregar evento
+    window.addEventListener('scroll', handleScroll);
 
-  //limpiar evento
-  
-  }
-  )
-  return(
-    <header className="fixed z-50 w-full">
-      <div className="container mx-auto">
+    //limpiar evento
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  },
+    []
+  );
+
+
+  return (
+    <header className={`fixed z-50 w-full 
+    ${active ? 'bg-[#030315] py-6' : 'bg-transparent py-8'}`}>
+      <div className="container mx-auto flex flex-col xl:flex-row items-center 
+      justify-between" >
         {/*logo */}
         <Link
-        href={'#'}
-        className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'
+          href={'#'}
+          className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'
         >
-          <Image src={'/assets/header/logo.svg'} fill/>
+          <Image src={'/assets/header/logo.svg'} 
+          fill
+          className="object-contain"
+          />
         </Link>
+
+        {/*nav */}
+        <nav>Nav</nav>
+        
+        {/*nav mobile */}
+        <nav>Nav mobile</nav>
+        
+        {/*menu btn */}
+        <nav>Menu btn</nav>
+        
+        {/*redes sociales */}
+        <nav>Redes Sociales</nav>
+
       </div>
-  </header>
+    </header>
   );
 };
 
