302d682ef4384c5d38f1747c636ef63a99065197
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-06 01:30:07 -0400

    agregando eventos

--- ingeelec/src/components/Header.jsx
+++ ingeelec/src/components/Header.jsx
@@ -9,7 +9,34 @@ import { fadeIn } from "../../variants";
 
 
 const Header = () => {
-  return <header className="fixed z-50 w-full">Header</header>;
+  const [active, setActive] = useState(false);
+
+  useEffect(() =>{
+    const handleScroll = () => {
+      // detectar scroll
+      setActive(window.scrollY > 100);
+    };
+
+  // agregar evento
+  window.addEventListener('scroll' , handleScroll);
+
+  //limpiar evento
+  
+  }
+  )
+  return(
+    <header className="fixed z-50 w-full">
+      <div className="container mx-auto">
+        {/*logo */}
+        <Link
+        href={'#'}
+        className='relative flex w-[226px] h-[37.64px] transition-all mb-4 xl:mb-0'
+        >
+          <Image src={'/assets/header/logo.svg'} fill/>
+        </Link>
+      </div>
+  </header>
+  );
 };
 
-export default Header
+export default Header;
