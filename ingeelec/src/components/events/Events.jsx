4959bf131eef864e25dbdd1c8bb5d6cd658927e8
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-05 21:07:10 -0400

    Eventos
    cabecera

--- ingeelec/src/components/events/Events.jsx
+++ ingeelec/src/components/events/Events.jsx
@@ -1,4 +1,5 @@
 import EventBox from './EventBox';
+import SectionHeader from './SectionHeader';
 
 const getEvents = async () => {
     const res = await fetch('http://localhost:4000/events');
@@ -12,6 +13,7 @@ const Events = async () => {
     return (
         <section className="section" id="tours">
             <div className="container mx-auto">
+                <SectionHeader pretitle='PRETITULOOOO' title='TITULOOOOOOO'/>
                 {/*even box*/}
                 <EventBox events={events} />
             </div>
