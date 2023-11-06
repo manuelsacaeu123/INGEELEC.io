c5413b6425b55d97c9a72d7df92af1b11b0a73c0
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-06 00:14:50 -0400

    interfaz inicio

--- ingeelec/src/components/Hero.jsx
+++ ingeelec/src/components/Hero.jsx
@@ -27,17 +27,84 @@ const Hero = () => {
         <div className='container mx-auto h-full flex justify-center items-center
         xl:justify-start'>
             {/*text */}
-            <div>text</div>
+            <div className='h-full flex flex-col justify-center items-center 
+            xl:items-start z-20 pt-12'>
+                <MouseParallaxContainer
+                    globalFactorX={0.1}
+                    globalFactorY={0.2}
+                    resetOnLeave
+                    className='relative flex items-center h-[120px] xl:h-max xl:w-[840px]'
+                >
+                    <MouseParallaxChild
+                        factorX={0.2}
+                        factorY={0.4}
+                        className='relative'
+
+                    >
+                        <motion.div 
+                        variants={fadeIn('up', 0.4)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className='w-[300px] h-[101.37px] xl-w-[725px] xl:h-[244.97]'>
+                            <Image src={'assets/hero/typo-1.svg'}
+                                fill
+                                alt=''
+                                className='object-contain'
+                            />
+                        </motion.div>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.9}
+                        factorY={0.9}
+                        className='absolute xl:left-6 z-30'
+
+                    >
+                        <motion.div 
+                        variants={fadeIn('up', 0.7)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className='w-[300px] h-[101.37px] xl-w-[625px] xl:h-[244.97]'>
+                            <Image src={'assets/hero/typo-2.svg'}
+                                fill
+                                alt=''
+                                className='object-contain'
+                            />
+                        </motion.div>
+                    </MouseParallaxChild>
+                    <MouseParallaxChild
+                        factorX={0.3}
+                        factorY={0.6}
+                        className='hidden xl:flex absolute right-0 z-20 opacity-80'
+
+                    >
+                        <motion.div 
+                        variants={fadeIn('left', 1.4)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className='w-[300px] h-[100px] xl-w-[248px] xl:h-[200]'>
+                            <Image src={'assets/hero/bird.png'}
+                                fill
+                                alt=''
+                                className='object-contain'
+                            />
+                        </motion.div>
+                    </MouseParallaxChild>
+
+                </MouseParallaxContainer>
+            </div>
             {/*imagne*/}
             <div className='hidden xl:flex absolute right-0 top-0 before:w-[748px]
             before:h-[893px] before:absolute before:right-0 before:top-0 
             before:bg-singerOverlay before:z-10'>
-                <Image src={'/assets/hero/singer.png'} 
-                width={617}
-                height={893} 
-                alt='' 
-                quality={100} 
-                priority
+                <Image src={'/assets/hero/singer.png'}
+                    width={617}
+                    height={893}
+                    alt=''
+                    quality={100}
+                    priority
                 />
             </div>
         </div>
