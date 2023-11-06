079c362906ef153b0df864a3d7512c22a842fb5f
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-05 21:06:49 -0400

    Cbezera
    empezando con la cabecera

---
+++ ingeelec/src/components/events/SectionHeader.jsx
@@ -0,0 +1,11 @@
+
+const SectionHeader = ({ pretitle, title }) => {
+    return (
+        <header>
+            <h3>{pretitle}</h3>
+            <h2>{title}</h2>
+        </header>
+    );
+};
+
+export default SectionHeader;
