916a320756ca5eb2d7129846f7732ef5bb59fe17
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-06 02:10:40 -0400

    barra de navegacion

--- ingeelec/src/components/Nav.jsx
+++ ingeelec/src/components/Nav.jsx
@@ -2,34 +2,44 @@ import { Link } from "react-scroll"
 
 const links = [
     {
-        path : 'home',
-        name : 'Home'
+        path: 'home',
+        name: 'Home'
     },
     {
-        path : 'home',
-        name : 'Home'
+        path: 'home',
+        name: 'Home'
     },
     {
-        path : 'home',
-        name : 'Home'
+        path: 'home',
+        name: 'Home'
     },
     {
-        path : 'home',
-        name : 'Home'
+        path: 'home',
+        name: 'Home'
     },
-    
+
 ]
 
-const Nav = ({containerStyles, linkStyles}) => {
-  return (
-    <nav>
-        {links.map((link, index) => {
-            return <Link to={link.path}>{link.name}</Link>;
-        })
+const Nav = ({ containerStyles, linkStyles }) => {
+    return (
+        <nav className={`${linkStyles}`}>
+            {links.map((link, index) => {
+                return (
+                    <Link to={link.path}
+                        className={`${linkStyles} cursor-pointer border-b-2 border-transparent`}
+                        key={index}
+                        smooth
+                        spy
+                        offset={-50}
+                        activeClass='active'
+                    >
+                        {link.name}
 
-        }
-    </nav>
-  )
+                    </Link>
+                );
+            })}
+        </nav>
+    )
 }
 
 export default Nav
