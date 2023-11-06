08daed7a1f334999ed8f515dc6c36d4c74af23df
manuelsacaeu123 <93751940+manuelsacaeu123@users.noreply.github.com>
2023-11-06 01:19:29 -0400

    header
    importando componentes

--- ingeelec/src/components/Header.jsx
+++ ingeelec/src/components/Header.jsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 
 const Header = () => {
   return (
